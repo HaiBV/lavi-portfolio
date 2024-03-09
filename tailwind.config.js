5f526f930df4ae125b0a8b5dfbc80033912c7f73
ADMIN-PC\Duc <duc@m.ibis.studio>
2024-03-09 23:28:27 +0700

    update works page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      translate: {
+        '-50%': '-50%',
+      },
+      fontSize: {
+        '110': '110px',
+        '21': '21px',
+        '68': '68px',
+      },
+      colors: {
+        'current': '#D0D1E7',
+      },
+      spacing: {
+        '20': '20%',
+        '10': '10%',
+        '40': '40%',
+        '90': '90%',
+        '-1.75rem': '-1.75rem',
+      }
     },
   },
   plugins: [],
