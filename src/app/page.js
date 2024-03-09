8169071aa48ff65e294c6b9e97eb423f747f3826
HaiBV <buivuhai.bk@gmail.com>
2024-03-09 09:41:19 +0700

    feat: update init data

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,20 @@
+import { works } from "@/data/works";
+
 export default function Home() {
   return (
     <main>
+      <h1>Home</h1>
+      <ul>
+        {works.map((work) => (
+          <li key={work.title}>
+            <h2>{work.title}</h2>
+            <p>{work.description}</p>
+            <p>{work.date}</p>
+            <p>{work.heroImage}</p>
+            <p>{work.caseStudy}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
---
+++ src/data/works.js
@@ -0,0 +1,39 @@
+export const works = [
+  {
+    title: "MATYEU",
+    description: "The first of AI-powered web-app for eyes healthcare",
+    date: "01/07/2024",
+    heroImage: "",
+    caseStudy: "#"
+  },{
+    title: "AON",
+    description: "Help their clients shape business decisions for the better",
+    date: "02/07/2023",
+    heroImage: "",
+    caseStudy: "#"
+  },{
+    title: "EXXON MOBIL",
+    description: "The largest publicly traded international oil & gas companies",
+    date: "03/07/2023",
+    heroImage: "",
+    caseStudy: "#"
+  },{
+    title: "GfE",
+    description: "More interactive lessons to faster and more personal feedback.",
+    date: "04/07/2023",
+    heroImage: "",
+    caseStudy: "#"
+  },{
+    title: "HALONG HUB",
+    description: "Enhancing experience on booking a cruise",
+    date: "05/07/2023",
+    heroImage: "",
+    caseStudy: "#"
+  },{
+    title: "GO FLY",
+    description: "Making the journey to learn English more joyful",
+    date: "06/07/2023",
+    heroImage: "",
+    caseStudy: "#"
+  },
+];
