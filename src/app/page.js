5de572e835dabb67450c5f08130a4216d1bdbc59
ADMIN-PC\Duc <duc@m.ibis.studio>
2024-04-02 12:52:30 +0700

    update animation work

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
             >
                 {works.map((work) => (
                     <SwiperSlide key={work.title} className={"relative before:absolute before:bg-[url('/img/hero/logo-slider.png')] before:bg-contain before:bg-center before:w-[100%] before:h-[100%] before:top-[50%] before:left-[50%] before:-translate-y-[50%] before:-translate-x-[50%] before:scale-0 before:z-10 before:duration-[3800ms] before:pointer-events-none"}>
-                        <div className={"item relative !h-screen !grid grid-cols-10 gap-x-6 auto-rows-max px-6 pt-[20.4vh] font-medium text-[68px] leading-none duration-[2000ms]"}>
+                        <div className={"relative !h-screen !grid grid-cols-10 gap-x-6 auto-rows-max px-6 pt-[20.4vh] font-medium text-[68px] leading-none"}>
                             <div className={"relative col-start-2 pl-7 before:absolute before:bg-current before:w-2 before:h-2 before:top-3 before:left-0"}></div>
                             <p className={"relative col-start-6 pl-7 before:absolute before:bg-current before:w-2 before:h-2 before:top-3 before:left-0"}>{work.date}</p>
                             <p className={"relative col-start-10 before:absolute before:bg-current before:w-2 before:h-2 before:top-3 before:-left-9"}>{work.century}</p>
@@ -46,7 +46,7 @@ export default function Home() {
                             <p className={"relative col-start-6 pl-10 before:absolute before:bg-current before:w-2 before:h-2 before:bottom-3 before:left-0"}>{work.month}</p>
                             <p className={"relative col-start-10 before:absolute before:bg-current before:w-2 before:h-2 before:bottom-3 before:-left-9"}>{work.year}</p>
                             <a href="#" className={"absolute text-[21px] border-b border-gray pb-3 right-6 top-[55.4%] -translate-y-[50%]"}>Case study</a>
-                            <div className={"item absolute -z-10 w-screen h-screen top-0 left-0"}>
+                            <div className={"img-slider absolute -z-10 w-screen h-screen top-0 left-0 duration-[2000ms]"}>
                                 <Image className={"absolute w-screen h-screen object-cover"} src={work.heroImage} alt="" width={1920} height={1080}></Image>
                             </div>
                         </div>
